f094c5f2e58f5b5910e0f8478d3ab8c0d5ffcf3b
Ureshipan <tiptevlad@yandex.ru>
2025-05-21 16:49:26 +0000

    some fixes of geometry

--- configs.py
+++ configs.py
@@ -85,57 +85,57 @@ def recalculate_vertices(params):
     l_mid = L - l_top - l_bot - vor_top - vor_bot
     sq_crcl = D / math.sqrt(2) / 2
 
-    crcl_smesh = 1
+    height = 30
 
     VERTICES = [
-        (-w_top/2, l_top + vor_top + l_mid / 2, 5),    # 0
-        (w_top/2, l_top + vor_top + l_mid / 2, 5),     # 1
-        (w_top/2, vor_top + l_mid / 2, 5),     # 2
-        (w_mid/2, l_mid/2, 5),       # 3
-        (w_mid/2, l_mid/2 - D - crcl_smesh * 2, 5),       # 4
-        (w_mid/2, -(l_mid/2 - D - crcl_smesh * 2), 5),      # 5
-        (w_mid/2, -l_mid/2, 5),      # 6
-        (w_bot/2, -(l_mid/2 + vor_bot), 5),    # 7
-        (w_bot/2, -(l_bot + vor_bot + l_mid / 2), 5),    # 8
-        (-w_bot/2, -(l_bot + vor_bot + l_mid / 2), 5),   # 9
-        (-w_bot/2, -(l_mid/2 + vor_bot), 5),   # 10
-        (-w_mid/2, -l_mid/2, 5),     # 11
-        (-w_mid/2, -(l_mid/2 - D - crcl_smesh * 2), 5),     # 12
-        (-w_mid/2, l_mid/2 - D - crcl_smesh * 2, 5),      # 13
-        (-w_mid/2, l_mid/2, 5),      # 14
-        (-w_top/2, vor_top + l_mid / 2, 5),    # 15
-        (-sq_crcl, l_mid/2 - crcl_smesh - (D/2 - sq_crcl), 5),  # 16
-        (sq_crcl, l_mid/2 - crcl_smesh - (D/2 - sq_crcl), 5),   # 17
-        (sq_crcl, l_mid/2 - crcl_smesh - D/2 - sq_crcl, 5),   # 18
-        (-sq_crcl, l_mid/2 - crcl_smesh - D/2 - sq_crcl, 5),  # 19
-        (-sq_crcl, -(l_mid/2 - crcl_smesh - D/2 - sq_crcl), 5), # 20
-        (sq_crcl, -(l_mid/2 - crcl_smesh - D/2 - sq_crcl), 5),  # 21
-        (sq_crcl, -(l_mid/2 - crcl_smesh - (D/2 - sq_crcl)), 5),  # 22
-        (-sq_crcl, -(l_mid/2 - crcl_smesh - (D/2 - sq_crcl)), 5), # 23
-        (-w_top/2, l_top + vor_top + l_mid / 2, -5),   # 24
-        (w_top/2, l_top + vor_top + l_mid / 2, -5),    # 25
-        (w_top/2, vor_top + l_mid / 2, -5),    # 26
-        (w_mid/2, l_mid/2, -5),      # 27
-        (w_mid/2, l_mid/2 - D - crcl_smesh * 2, -5),      # 28
-        (w_mid/2, -(l_mid/2 - D - crcl_smesh * 2), -5),     # 29
-        (w_mid/2, -l_mid/2, -5),     # 30
-        (w_bot/2, -(l_mid/2 + vor_bot), -5),   # 31
-        (w_bot/2, -(l_bot + vor_bot + l_mid / 2), -5),   # 32
-        (-w_bot/2, -(l_bot + vor_bot + l_mid / 2), -5),  # 33
-        (-w_bot/2, -(l_mid/2 + vor_bot), -5),  # 34
-        (-w_mid/2, -l_mid/2, -5),    # 35
-        (-w_mid/2, -(l_mid/2 - D - crcl_smesh * 2), -5),    # 36
-        (-w_mid/2, l_mid/2 - D - crcl_smesh *   2, -5),     # 37
-        (-w_mid/2, l_mid/2, -5),     # 38
-        (-w_top/2, vor_top + l_mid / 2, -5),   # 39
-        (-sq_crcl, l_mid/2 - crcl_smesh - (D/2 - sq_crcl), -5), # 40
-        (sq_crcl, l_mid/2 - crcl_smesh - (D/2 - sq_crcl), -5),  # 41
-        (sq_crcl, l_mid/2 - crcl_smesh - D/2 - sq_crcl, -5),  # 42
-        (-sq_crcl, l_mid/2 - crcl_smesh - D/2 - sq_crcl, -5), # 43
-        (-sq_crcl, -(l_mid/2 - crcl_smesh - D/2 - sq_crcl), -5),# 44
-        (sq_crcl, -(l_mid/2 - crcl_smesh - D/2 - sq_crcl), -5), # 45
-        (sq_crcl, -(l_mid/2 - crcl_smesh - (D/2 - sq_crcl)), -5), # 46
-        (-sq_crcl, -(l_mid/2 - crcl_smesh - (D/2 - sq_crcl)), -5) # 47
+        (-w_top/2, l_top + vor_top + l_mid / 2, height/2),    # 0
+        (w_top/2, l_top + vor_top + l_mid / 2, height/2),     # 1
+        (w_top/2, vor_top + l_mid / 2, height/2),     # 2
+        (w_mid/2, l_mid/2, height/2),       # 3
+        (w_mid/2, l_mid/2 - w_mid, height/2),       # 4
+        (w_mid/2, -(l_mid/2 - w_mid), height/2),      # 5
+        (w_mid/2, -l_mid/2, height/2),      # 6
+        (w_bot/2, -(l_mid/2 + vor_bot), height/2),    # 7
+        (w_bot/2, -(l_bot + vor_bot + l_mid / 2), height/2),    # 8
+        (-w_bot/2, -(l_bot + vor_bot + l_mid / 2), height/2),   # 9
+        (-w_bot/2, -(l_mid/2 + vor_bot), height/2),   # 10
+        (-w_mid/2, -l_mid/2, height/2),     # 11
+        (-w_mid/2, -(l_mid/2 - w_mid), height/2),     # 12
+        (-w_mid/2, l_mid/2 - w_mid, height/2),      # 13
+        (-w_mid/2, l_mid/2, height/2),      # 14
+        (-w_top/2, vor_top + l_mid / 2, height/2),    # 15
+        (-sq_crcl, l_mid/2 - w_mid / 2 + sq_crcl, height/2),  # 16
+        (sq_crcl, l_mid/2 - w_mid / 2 + sq_crcl, height/2),   # 17
+        (sq_crcl, l_mid/2 - w_mid / 2 - sq_crcl, height/2),   # 18
+        (-sq_crcl, l_mid/2 - w_mid / 2 - sq_crcl, height/2),  # 19
+        (-sq_crcl, -(l_mid/2 - w_mid / 2 - sq_crcl), height/2), # 20
+        (sq_crcl, -(l_mid/2 - w_mid / 2 - sq_crcl), height/2),  # 21
+        (sq_crcl, -(l_mid/2 - w_mid / 2 + sq_crcl), height/2),  # 22
+        (-sq_crcl, -(l_mid/2 - w_mid / 2 + sq_crcl), height/2), # 23
+        (-w_top/2, l_top + vor_top + l_mid / 2, -height/2),   # 24
+        (w_top/2, l_top + vor_top + l_mid / 2, -height/2),    # 25
+        (w_top/2, vor_top + l_mid / 2, -height/2),    # 26
+        (w_mid/2, l_mid/2, -height/2),      # 27
+        (w_mid/2, (l_mid/2 - w_mid), -height/2),      # 28
+        (w_mid/2, -(l_mid/2 - w_mid), -height/2),     # 29
+        (w_mid/2, -l_mid/2, -height/2),     # 30
+        (w_bot/2, -(l_mid/2 + vor_bot), -height/2),   # 31
+        (w_bot/2, -(l_bot + vor_bot + l_mid / 2), -height/2),   # 32
+        (-w_bot/2, -(l_bot + vor_bot + l_mid / 2), -height/2),  # 33
+        (-w_bot/2, -(l_mid/2 + vor_bot), -height/2),  # 34
+        (-w_mid/2, -l_mid/2, -height/2),    # 35
+        (-w_mid/2, -(l_mid/2 - w_mid), -height/2),    # 36
+        (-w_mid/2, (l_mid/2 - w_mid), -height/2),     # 37
+        (-w_mid/2, l_mid/2, -height/2),     # 38
+        (-w_top/2, vor_top + l_mid / 2, -height/2),   # 39
+        (-sq_crcl, (l_mid/2 - w_mid / 2 + sq_crcl), -height/2), # 40
+        (sq_crcl, (l_mid/2 - w_mid / 2 + sq_crcl), -height/2),  # 41
+        (sq_crcl, (l_mid/2 - w_mid / 2 - sq_crcl), -height/2),  # 42
+        (-sq_crcl, (l_mid/2 - w_mid / 2 - sq_crcl), -height/2), # 43
+        (-sq_crcl, -(l_mid/2 - w_mid / 2 - sq_crcl), -height/2),# 44
+        (sq_crcl, -(l_mid/2 - w_mid / 2 - sq_crcl), -height/2), # 45
+        (sq_crcl, -(l_mid/2 - w_mid / 2 + sq_crcl), -height/2), # 46
+        (-sq_crcl, -(l_mid/2 - w_mid / 2 + sq_crcl), -5) # 47
     ]
 
 
@@ -227,45 +227,45 @@ convertToMeters 0.01;
 
 blocks
 (
-    hex (24 39 26 25 0 15 2 1) (10 5 1) simpleGrading (1 1 1) // 1
-    hex (39 38 27 26 15 14 3 2) (10 5 1) simpleGrading (1 1 1) // 2
+    hex (24 39 26 25 0 15 2 1) (10 10 1) simpleGrading (1 1 1) // 1
+    hex (39 38 27 26 15 14 3 2) (10 10 1) simpleGrading (1 1 1) // 2
     
-    hex (38 40 41 27 14 16 17 3) (5 5 1) simpleGrading (1 1 0.1) // 3
-    hex (38 37 43 40 14 13 19 16) (5 5 1) simpleGrading (1 1 0.1) // 4
-    hex (41 42 28 27 17 18 4 3) (5 5 1) simpleGrading (1 1 0.1) // 5
-    hex (43 37 28 42 19 13 4 18) (5 5 1) simpleGrading (1 1 0.1) // 6
+    hex (38 40 41 27 14 16 17 3) (10 10 1) simpleGrading (1 1 0.1) // 3
+    hex (38 37 43 40 14 13 19 16) (10 10 1) simpleGrading (1 1 0.1) // 4
+    hex (41 42 28 27 17 18 4 3) (10 10 1) simpleGrading (1 1 0.1) // 5
+    hex (43 37 28 42 19 13 4 18) (10 10 1) simpleGrading (1 1 0.1) // 6
     
-    hex (37 36 29 28 13 12 5 4) (10 5 1) simpleGrading (1 1 1) // 7
+    hex (37 36 29 28 13 12 5 4) (10 10 1) simpleGrading (1 1 1) // 7
     
-    hex (36 44 45 29 12 20 21 5) (5 5 1) simpleGrading (1 1 0.1) // 8
-    hex (36 35 47 44 12 11 23 20) (5 5 1) simpleGrading (1 1 0.1) // 9
-    hex (45 46 30 29 21 22 6 5) (5 5 1) simpleGrading (1 1 0.1) // 10
-    hex (47 35 30 46 23 11 6 22) (5 5 1) simpleGrading (1 1 0.1) // 11
+    hex (36 44 45 29 12 20 21 5) (10 10 1) simpleGrading (1 1 0.1) // 8
+    hex (36 35 47 44 12 11 23 20) (10 10 1) simpleGrading (1 1 0.1) // 9
+    hex (45 46 30 29 21 22 6 5) (10 10 1) simpleGrading (1 1 0.1) // 10
+    hex (47 35 30 46 23 11 6 22) (10 10 1) simpleGrading (1 1 0.1) // 11
     
-    hex (35 34 31 30 11 10 7 6) (10 5 1) simpleGrading (1 1 1) // 12
-    hex (34 33 32 31 10 9 8 7) (10 5 1) simpleGrading (1 1 1) // 13
+    hex (35 34 31 30 11 10 7 6) (10 10 1) simpleGrading (1 1 1) // 12
+    hex (34 33 32 31 10 9 8 7) (10 10 1) simpleGrading (1 1 1) // 13
 );
 
 edges
 (
     // top circle
- arc 16 17 (0 {VERTICES[3][1]-1}7 5)
- arc 40 41 (0 {VERTICES[3][1]-1} -5)
- arc 17 18 ({params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} 5)
- arc 41 42 ({params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} -5)
- arc 18 19 (0 {VERTICES[4][1]+1} 5)
- arc 42 43 (0 {VERTICES[4][1]+1} -5)
- arc 19 16 (-{params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} 5)
- arc 43 40 (-{params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} -5)
+ arc 16 17 (0 {(VERTICES[18][1]+VERTICES[17][1]) / 2 + params["D"]/2} {VERTICES[0][2]})
+ arc 40 41 (0 {(VERTICES[18][1]+VERTICES[17][1]) / 2 + params["D"]/2} -{VERTICES[0][2]})
+ arc 17 18 ({params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} {VERTICES[0][2]})
+ arc 41 42 ({params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} -{VERTICES[0][2]})
+ arc 18 19 (0 {(VERTICES[18][1]+VERTICES[17][1]) / 2 - params["D"]/2} {VERTICES[0][2]})
+ arc 42 43 (0 {(VERTICES[18][1]+VERTICES[17][1]) / 2 - params["D"]/2} -{VERTICES[0][2]})
+ arc 19 16 (-{params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} {VERTICES[0][2]})
+ arc 43 40 (-{params["D"]/2} {(VERTICES[17][1]+VERTICES[18][1])/2} -{VERTICES[0][2]})
     // bot circle
- arc 22 23 (0 -{VERTICES[3][1]-1} 5)
- arc 46 47 (0 -{VERTICES[3][1]-1} -5)
- arc 21 22 ({params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} 5)
- arc 45 46 ({params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} -5)
- arc 20 21 (0 -{VERTICES[4][1]+1} 5)
- arc 44 45 (0 -{VERTICES[4][1]+1} -5)
- arc 23 20 (-{params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} 5)
- arc 47 44 (-{params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} -5)
+ arc 22 23 (0 -{(VERTICES[18][1]+VERTICES[17][1]) / 2 + params["D"]/2} {VERTICES[0][2]})
+ arc 46 47 (0 -{(VERTICES[18][1]+VERTICES[17][1]) / 2 + params["D"]/2} -{VERTICES[0][2]})
+ arc 21 22 ({params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} {VERTICES[0][2]})
+ arc 45 46 ({params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} -{VERTICES[0][2]})
+ arc 20 21 (0 -{(VERTICES[18][1]+VERTICES[17][1]) / 2 - params["D"]/2} {VERTICES[0][2]})
+ arc 44 45 (0 -{(VERTICES[18][1]+VERTICES[17][1]) / 2 - params["D"]/2} -{VERTICES[0][2]})
+ arc 23 20 (-{params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} {VERTICES[0][2]})
+ arc 47 44 (-{params["D"]/2} -{(VERTICES[17][1]+VERTICES[18][1])/2} -{VERTICES[0][2]})
 );
 
 boundary
